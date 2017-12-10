08953f1fc34dbef199cd28fa1bcaaed314113624
t0biwan <tobi.klatt@web.de>
2017-12-10 15:11:41 +0100

    Now using the up to date version of bulma..

--- jedha-frontend/src/App.vue
+++ jedha-frontend/src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div id="app">
+<div id="app">
+  <hero title="Jedha" subtitle="We fight the lack of labeled datasets for deep learning through distributed user input"></hero>
+  <div class="container">
+    <div class="box">
+      <div class="field has-addons has-addons-centered">
+        <div class="control is-expanded">
+          <input class="input" type="text" placeholder="Label">
+        </div>
+        <div class="control">
+          <a class="button is-info">Send</a>
+        </div>
+      </div>
+    </div>
+
   </div>
+</div>
+</div>
 </template>
 
 <script>
+import hero from "./components/hero.vue"
+
 export default {
   name: 'app',
+  components: {
+    'hero': hero,
+  },
 }
 </script>
 

2cfb4c9b1d491544b57f9a361429b86c0d3c03c3
Julius Lange <julius_lange@web.de>
2017-12-11 17:23:29 +0100

    implemented and tested the method to perform shift (image augmentation); added a folder for usefull stuff containing a cheat sheet for numpy

--- DL/data_augmentation.py
+++ DL/data_augmentation.py
@@ -6,10 +6,19 @@
 # imports: 
 import numpy as np
 
-# shift the pixels of the image by a specified amount, an fill with zeros
-def shift(image, x_shift, y_shift):
-	# TODO: add functionality
-	return shifted_image
+# shift the pixels of the image by a specified amount, and fill with zeros; positive and negative parameters possible
+# WORKS FOR GREYSCALE ONLY (only one color channel expected)
+def shift(image, y_shift, x_shift):
+	# get image shape and magnitude of shifts:
+	w,h,d = image.shape
+	d_x = abs(x_shift)
+	d_y = abs(y_shift)
+	# create new image with increased shape (increased by twice the shift magnitudes):
+	shifted_image = np.zeros((w+2*d_x,h+2*d_y,d))
+	# copy values of the original image to the center of the new image:
+	shifted_image[d_x:w+d_x, d_y:h+d_y,:] = image
+	# cut and return the new image according to the positive/negative shift parameters:
+	return shifted_image[d_x-x_shift:w+d_x-x_shift, d_y-y_shift:h+d_y-y_shift, :]
 
 # rotate the pixels by a specified angle, rotational center = image center
 def rotate(image, angle):
@@ -32,4 +41,12 @@ def noise(image, intensity):
 	return noised_image
 
 # TODO: add more functions
+
+a = np.ones((3,4))
+a = a.reshape((3,4,1))
+print(a)
+print("---")
+b = shift(a,2,0)
+print(b.reshape(3,4))
+
 
